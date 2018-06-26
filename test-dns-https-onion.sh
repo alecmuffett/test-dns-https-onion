e386b5d25797ca5e69b3d7e06bbe9e36137609ba
Alec Muffett <alec.muffett@gmail.com>
2018-06-26 13:44:41 +0100

    commit: nits

--- test-dns-https-onion.sh
+++ test-dns-https-onion.sh
@@ -8,17 +8,17 @@
 
 TARGET="www.openrightsgroup.org"
 
-TOR_PROXY="127.0.0.1:9150"
+TOR_PROXY="127.0.0.1:9150" # amend this, if your tor proxy is elsewhere
 
 while read src url curl_opts; do
+    echo ""
     echo :::: testing $src ::::
     curl -H 'accept: application/dns-json' $curl_opts $url
-    echo ""
     echo ""
 done <<EOF
 google      https://dns.google.com/resolve?name=$TARGET
 cloudflare  https://cloudflare-dns.com/dns-query?name=$TARGET
-dns_onion   https://dns4torpnlfs2ifuz2s2yf3fc7rdmsbhm6rw75euj35pac6ap25zgqad.onion/dns-query?name=$TARGET -x socks5h://$TOR_PROXY/
+cf_onion    https://dns4torpnlfs2ifuz2s2yf3fc7rdmsbhm6rw75euj35pac6ap25zgqad.onion/dns-query?name=$TARGET -x socks5h://$TOR_PROXY/
 EOF
 
 exit 0
