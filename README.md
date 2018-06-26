5cd8d542c2a8639344bbc0299a530adc3bba830e
Alec Muffett <alec.muffett@gmail.com>
2018-06-26 13:37:25 +0100

    commit: initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# test-dns-https-onion
-Testing DNS-over-HTTPS-over-Onion
+# Testing DNS-over-HTTPS-over-Onion (DHO? DNSHO?)
+
+Explanation: https://twitter.com/AlecMuffett/status/1011504656667770880
+
+v1.0 - alec.muffett@gmail.com 2018-06-26
---
+++ test-dns-https-onion.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# testing DNS-over-HTTPS-over-Onion (DHO? DNSHO?)
+# explanation: https://twitter.com/AlecMuffett/status/1011504656667770880
+# v1.0 - alec.muffett@gmail.com 2018-06-26
+
+# You need to have TorBrowser running locally to provide a SOCKS5
+# relay to Tor on port 9150 (see below) for the Onion lookup to work.
+
+TARGET="www.openrightsgroup.org"
+
+TOR_PROXY="127.0.0.1:9150"
+
+while read src url curl_opts; do
+    echo :::: testing $src ::::
+    curl -H 'accept: application/dns-json' $curl_opts $url
+    echo ""
+    echo ""
+done <<EOF
+google      https://dns.google.com/resolve?name=$TARGET
+cloudflare  https://cloudflare-dns.com/dns-query?name=$TARGET
+dns_onion   https://dns4torpnlfs2ifuz2s2yf3fc7rdmsbhm6rw75euj35pac6ap25zgqad.onion/dns-query?name=$TARGET -x socks5h://$TOR_PROXY/
+EOF
+
+exit 0
